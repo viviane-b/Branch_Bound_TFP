06b50e41dd6b9320a383374702c0a4ade4580d12
Viviane Binet <vivianebinet@hotmail.com>
2025-10-18 22:14:22 -0400

    gurobi solver on smaller set

--- quadratic_programming.py
+++ quadratic_programming.py
@@ -1,8 +1,10 @@
 # a
+import numpy as np
 from fontTools.misc.cython import returns
 from gurobipy import Model, GRB
 import pandas as pd
-
+import scipy
+from numpy.ma.core import shape
 
 
 ## Quadratic programming example
@@ -46,6 +48,60 @@ def test():
 
 
 # Import data
+dim = 1021
+small_dim = 50
+
 dist = pd.read_csv("TFP-data-master/imdbSPdist3digits.txt", header=None, index_col=False)
-dist = dist.drop(dist.columns[1021], axis=1)
+dist = dist.drop(dist.columns[dim], axis=1)
 print(dist)
+
+small_dist = dist.iloc[:small_dim, :small_dim]
+print(small_dist)
+
+
+P =  np.triu(dist, k=1)
+small_P = np.triu(small_dist, k=1)
+print(small_P)
+
+
+skills = pd.read_csv("TFP-data-master/imdbskills.txt", sep="\t", header=None, index_col=False)
+skills.fillna(0, inplace=True)
+skills = skills.astype(int)
+small_skills = skills.iloc[:small_dim]
+print(small_skills)
+print(skills[5].T)
+print("data types")
+vector = small_skills[5].values
+print(vector.shape)
+
+# N_size: size of set of candidates
+# req_skills: set of skills required on the team
+def first_model(N_size, req_skills):
+
+    # model
+    m = Model("first")
+
+    # Create variables
+    y = m.addMVar(shape=N_size, vtype=GRB.BINARY, name="y")
+    print(y.shape)
+
+    # Set objective
+    obj = (small_P@y)@y
+    m.setObjective(obj)
+
+    # Add constraint: a skill is covered
+
+    for skill_no in req_skills:
+
+        m.addConstr(small_skills[skill_no].values @ y >= 1 )
+
+
+    m.optimize()
+
+    for v in m.getVars():
+        print(f"{v.VarName} {v.X:g}")
+
+    print(f"Obj: {m.ObjVal:g}")
+
+
+first_model(small_dim, [3,4,5,6,11,12,19,20,23,26])
